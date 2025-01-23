02278ee50f154778fab1e963bf1786d6c640ad46
wei-song1 <weisong141@hotmail.com>
2025-01-23 11:45:28 +0800

    Added the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Codsworth.java
+++ src/main/java/Codsworth.java
@@ -3,22 +3,38 @@ import java.util.Scanner;
 public class Codsworth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] strTasks = new String[100];
+        int i = 0;
         boolean isBye = false;
 
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello, I'm Codsworth");
         System.out.println("    What can I do for you?");
         System.out.println("    ____________________________________________________________");
+
         while(!isBye) {
             String strInput = sc.nextLine();
-            if (strInput.equals("bye")) {
-                isBye = true;
-            } else {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("    " + strInput);
-                System.out.println("    ____________________________________________________________");
+            switch (strInput) {
+                case "list":
+                    System.out.println("    ____________________________________________________________");
+                    for (int j = 0; j < i; j++) {
+                        System.out.println("    " + (j + 1) + ". " + strTasks[j]);
+                    }
+                    System.out.println("    ____________________________________________________________");
+                    break;
+                case "bye":
+                    isBye = true;
+                    break;
+                default:
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("    added: " + strInput);
+                    System.out.println("    ____________________________________________________________");
+                    strTasks[i] = strInput;
+                    i++;
+                    break;
             }
         }
+
         System.out.println("    ____________________________________________________________");
         System.out.println("    Bye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
