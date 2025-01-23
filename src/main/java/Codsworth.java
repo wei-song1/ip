4f089923e444858bf3933b4d0d4307df86656724
wei-song1 <weisong141@hotmail.com>
2025-01-23 11:35:35 +0800

    Modified Codsworth.java to Echo

--- src/main/java/Codsworth.java
+++ src/main/java/Codsworth.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Codsworth {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello, I'm Codsworth");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        boolean isBye = false;
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello, I'm Codsworth");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        while(!isBye) {
+            String strInput = sc.nextLine();
+            if (strInput.equals("bye")) {
+                isBye = true;
+            } else {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("    " + strInput);
+                System.out.println("    ____________________________________________________________");
+            }
+        }
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+        sc.close();
     }
 }
